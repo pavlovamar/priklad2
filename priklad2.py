f8bc33f52db43e9d50ee695b696ed08f03e8f5ec
pavlovamar <116712972+pavlovamar@users.noreply.github.com>
2023-02-10 10:51:10 +0100

    Correction

--- priklad2.py
+++ priklad2.py
@@ -2,28 +2,28 @@ import sys
 
 class Text:
     def __init__(self):
-        self.__text__ = None
-        self.__vowels__ = 0
-        self.__consonants__ = 0
-        self.__vowel__ = [65, 69, 73, 79, 85, 89, 97, 101, 105, 111, 117, 121]
-        self.__vowels_with_diacritics__ = [193, 201, 205, 211, 218, 221, 225, 233, 237, 243, 250, 253, 282, 283, 366, 367]
-        self.__consonants_with_diacritics__ = [268, 269, 270, 271, 327, 328, 344, 345, 352, 353, 356, 357, 381, 382]
+        self.__text = None
+        self.__vowels = 0
+        self.__consonants = 0
+        self.__vowel = [65, 69, 73, 79, 85, 89, 97, 101, 105, 111, 117, 121]
+        self.__vowels_with_diacritics = [193, 201, 205, 211, 218, 221, 225, 233, 237, 243, 250, 253, 282, 283, 366, 367]
+        self.__consonants_with_diacritics = [268, 269, 270, 271, 327, 328, 344, 345, 352, 353, 356, 357, 381, 382]
 
     def count_letters(self):
-        if self.__text__ == None:
+        if self.__text == None:
             return print("There is no data.")
-        for letter in self.__text__:
-            if ord(letter) in self.__vowel__ or ord(letter) in self.__vowels_with_diacritics__:
-                self.__vowels__ += 1
-            elif ord(letter) in self.__consonants_with_diacritics__:
-                self.__consonants__ +=1
+        for letter in self.__text:
+            if ord(letter) in self.__vowel or ord(letter) in self.__vowels_with_diacritics:
+                self.__vowels += 1
+            elif ord(letter) in self.__consonants_with_diacritics:
+                self.__consonants +=1
             elif (ord(letter)>=65 and ord(letter)<=90) or (ord(letter)>=97 and ord(letter)<=122):
-                self.__consonants__ += 1
+                self.__consonants += 1
 
     def load(self,adress):
         try:
             with open (adress, 'r', encoding = "utf-8") as input:
-                self.__text__ = list(input.read())
+                self.__text = list(input.read())
         except FileNotFoundError:
             sys.exit ("The file has not been found.")
         except PermissionError:
@@ -36,9 +36,13 @@ class Text:
             sys.exit ("Something went wrong.")
     def save(self,adress):
         with open (adress, 'a', encoding = "utf-8") as output:
-            output.write('\n\n'f"The are {self.__vowels__} vowels and {self.__consonants__} consonants in the text.")
-    
-object = Text()
-object.load("input.txt")
-object.count_letters()
-object.save("input.txt")
+            output.write('\n\n'f"The are {self.__vowels} vowels and {self.__consonants} consonants in the text.")
+
+
+def main():
+    object = Text()
+    object.load("input.txt")
+    object.count_letters()
+    object.save("input.txt")
+
+main()
